41d772313d5fcffb6b73ae93f8dcb6312f8567f9
energos <energos@users.noreply.github.com>
2020-06-27 00:27:15 -0300

    avr/simple09: break the line into words

--- simpleC/avr/simple09/simple.c
+++ simpleC/avr/simple09/simple.c
@@ -1,14 +1,11 @@
-/* embeddedC/simpleC/avr/simple08/simple.c */
+/* embeddedC/simpleC/avr/simple09/simple.c */
 
-/* Leitura de linha pela UART.
+/* Leitura de linha pela UART e separação em "palavras".
 
-   A rotina GetLine() oferece uma edição de linha mínima e retorna:
-   - 0 se a linha não está completa, ou seja, ainda não foi teclado <ENTER>
-   - n+1 se a linha está completa, onde n é o número de caracteres da linha
-   Mais ou menos UTF-8 compatível...
+   A rotina breakLine() "quebra" uma linha lida da UART em "palavras" e retorna
+   ponteiros para o início destas "palavras".
 
-   Para teste, o programa apenas chama a rotina GetLine() e ecoa o conteúdo da
-   linha lida. */
+   Para teste, o programa lê a linha e ecoa separadamente as "palavras". */
 
 /* baudrate */
 #define BAUDRATE 9600
@@ -88,6 +85,7 @@ void uart_puts_P(const char* s);
 unsigned char uart_kbhit(void);
 char uart_getchar(void);
 unsigned char GetLine(void);
+void breakLine(void);
 
 
 char UART_RxBuffer[UART_RX_BUFFER_SIZE];
@@ -144,6 +142,14 @@ char uart_getchar(void)
 /* número de caracteres no buffer de edição da linha de comando + null */
 #define LINE_BUFFER_SIZE 28
 char LineBuffer[LINE_BUFFER_SIZE];
+
+/* número máximo de argumentos na linha de comando */
+#define ARGS_MAX 4
+/* array de pointers para strings onde estão os elementos da linha de comando */
+char *Args[ARGS_MAX];
+/* número de argumentos na linha de comando */
+unsigned char ArgsN;
+
 
 /*---------------------------------------------------------------------------*
  * unsigned char GetLine(void)
@@ -204,6 +210,57 @@ unsigned char GetLine(void)
   return nchars;
 }
 
+/*---------------------------------------------------------------------------*
+ * void breakLine(void)
+ *
+ * Separa em palavras a linha em LineBuffer
+ * Palavra é qualquer conjunto de caracteres delimitado por um ou mais espaços
+ * Espaço ou qualquer controle é aceito como delimitador de palavra
+ *
+ * a linha de comando será quebrada em até ARGS_MAX palavras (argumentos)
+ * Args[0] apontará para o primeiro argumento (comando)
+ * Args[1] apontará para o segundo  argumento (primeiro parâmetro)
+ * ...
+ * Args[3] apontará para o quarto   argumento (terceiro parâmetro)
+ *
+ * Retorno
+ *    número de argumentos em ArgsN
+ *    pointers para até ARGS_MAX argumentos em Args[0], Args[1], ...
+ *
+ * Efeito colateral
+ *    altera o conteúdo de LineBuffer, os espaços são substituidos por \0
+ */
+void breakLine(void)
+{
+  char *p = LineBuffer;
+  char c;
+  unsigned char firstchar = 1;
+  unsigned char n = 0;
+
+  while((c = *p))
+    {
+      if((unsigned char)c <= ' ')
+        {
+          firstchar = 1;
+          *p = 0;
+        }
+      else
+        {
+          if(firstchar)
+            {
+              if(n >= ARGS_MAX) break;
+              Args[n] = p;
+              n++;
+              firstchar = 0;
+            }
+        }
+      p++;
+    }
+  ArgsN = n;
+}
+
+/*---------------------------------------------------------------------------*
+ */
 int main(void)
 {
   unsigned char n = 1;
@@ -245,9 +302,13 @@ int main(void)
         {
           if(n > 1)
             {
-              uart_puts_P(PSTR("\nVocê digitou: ->"));
-              uart_puts(LineBuffer);
-              uart_puts_P(PSTR("<-"));
+              uart_puts_P(PSTR("\nVocê digitou:"));
+              breakLine();
+              for(n = 0; n < ArgsN; n++)
+                {
+                  uart_putchar('\n');
+                  uart_puts(Args[n]);
+                }
             }
           uart_puts_P(PSTR("\n> "));
         }
